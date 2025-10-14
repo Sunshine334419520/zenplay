1b486e46615dbd92a6bed1f6f6bcb27d8db6debd
Yang Guang <guang334419520@126.com>
2025-10-14 12:26:39 +0000

    update

--- src/player/playback_controller.cpp
+++ src/player/playback_controller.cpp
@@ -521,7 +521,7 @@ void PlaybackController::SyncControlTask() {
     }
 
     // 100Hz更新频率
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 }
 
